8f2335a0f0c5f4ba4eaf666ef42f4b57572cf4f6
Mark Tiu <mark.tiu@prudentialguarantee.com>
2026-03-08 12:39:39 +0800

    Initial commit

---
+++ src/main/java/com/prudentialguarantee/paymentrouter/PaymentRouterApp.java
@@ -0,0 +1,13 @@
+package com.prudentialguarantee.paymentrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRouterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentRouterApp.class, args);
+    }
+
+}
